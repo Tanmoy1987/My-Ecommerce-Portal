6d1d3a8c761327b00e2216d5d430c9d32e8e4e61
Tanmoy1987 <tanmoy.saha87@gmail.com>
2022-01-03 13:15:55 +0530

    Product Catalog

--- organic-shop/src/app/product/product/product.component.ts
+++ organic-shop/src/app/product/product/product.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Categories } from 'src/app/model/categories';
+import { Product } from 'src/app/model/product';
+import { CategoryService } from 'src/app/service/category.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
+  products: Product[]= [];
+  //productList$!: Observable<Product[]>;
+  categories$!: Observable<Categories[]>;
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute
+             , private categoryService: CategoryService
+             , private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.categories$= this.categoryService.getCategories();
+    //this.productList$= this.productService.getAllProducts();
+    this.subscription= this.route.queryParamMap
+         .pipe(switchMap(params => this.productService.getAllProducts()
+          .pipe(map(products => { 
+           return { products, params }
+          })
+        ) 
+      )
+    ).subscribe(d => {
+       if(d.params.get('category') == null) {
+          this.products= d.products;
+          return;
+       }
+       this.products= d.products.filter(y => y.category == d.params.get('category'))    
+    });
+ }
+ ngOnDestroy(): void {
+  this.subscription.unsubscribe();
+ }
+  counter(cn: number){
+    return new Array(Math.ceil(cn/2));
   }
-
 }
