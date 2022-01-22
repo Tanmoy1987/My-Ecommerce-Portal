b627861024e8c46971d6ee6978c4fe15e89ef7a4
Tanmoy1987 <tanmoy.saha87@gmail.com>
2022-01-23 05:12:28 +0530

    minor modification

--- organic-shop/src/app/app.component.ts
+++ organic-shop/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators';
+import { of, from } from 'rxjs';
+import { delay, map, mergeMap, switchMap } from 'rxjs/operators';
 import { AuthService } from './service/auth.service';
+
 //import { AngularFireAuth } from '@angular/fire/compat/auth';
 //import { AngularFireDatabase } from '@angular/fire/compat/database';
 //import * as firebase from 'firebase/auth' 
@@ -18,18 +20,24 @@ export class AppComponent {
   let returnURL = localStorage.getItem('returnUrl');
   localStorage.removeItem('returnUrl');
 
-  this.auth.user$.pipe(switchMap(fu => this.auth.get(fu)
-        .pipe(map(user => {
-            return { fu, user }
+  this.auth.user$.pipe(switchMap(user => this.auth.get(user)
+        .pipe(map(appUser => {
+            return { user, appUser }
       })
     )
   )
- ).subscribe(obj => {
-      if(!obj.fu) return;
-      AppComponent._userKey= obj.fu?.uid;
-      if(!obj.user) this.auth.create(obj.fu);
+  ).subscribe((obj) => {
+      if(!obj.user) return;
+      AppComponent._userKey= obj.user?.uid;
+      if(!obj.appUser) this.auth.create(obj.user);
       if(returnURL) this.router.navigateByUrl(returnURL);
-    });
+   });
 
+   //this.name$.pipe(mergeMap(arr => this.dummyApi(arr))).subscribe(d => console.log(d));
  }
+ /*dummyApi= function(ch: string) {
+   return of(`API response for the character ${ch}`).pipe(delay(1000));
+ };
+ name$= from(['Tanmoy','Sayanti','Laxmi','Rakesh','Somnath']);*/
+
 }
